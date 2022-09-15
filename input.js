f268c47918de338807f81fd83e8a756cc64952fd
Alexander Kostin <asurv2@gmail.com>
2022-09-15 16:16:55 +0300

    Create input.js

---
+++ input.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const eventCallback = function (e) {
+        let el = e.target,
+            clearVal = el.dataset.phoneClear,
+            pattern = el.dataset.phonePattern,
+            matrix_def = "+__(___) ___-__-__",
+            matrix = pattern ? pattern : matrix_def,
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = e.target.value.replace(/\D/g, "");
+
+        if (clearVal !== "false" && e.type === "blur") {
+            if (val.length < matrix.match(/([\_\d])/g).length) {
+                e.target.value = "";
+                return;
+            }
+        }
+        if (def.length >= val.length) val = def;
+        e.target.value = matrix.replace(/./g, function (e) {
+            return /[_\d]/.test(e) && i < val.length
+                ? val.charAt(i++)
+                : i >= val.length
+                ? ""
+                : e;
+        });
+    };
+    const numbers = document.querySelectorAll("[data-phone-pattern]");
+    for (let number of numbers) {
+        for (let e of ["input", "blur", "focus"]) {
+            number.addEventListener(e, eventCallback);
+        }
+    }
+});
