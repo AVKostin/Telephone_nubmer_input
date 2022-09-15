5c6e1700a2c18985095180bd2a888b263473f44a
Alexander Kostin <asurv2@gmail.com>
2022-09-15 17:43:40 +0300

    bugfix

--- input.js
+++ input.js
@@ -1,20 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     const eventCallback = function (e) {
         let el = e.target,
-            clearVal = el.dataset.phoneClear,
             pattern = el.dataset.phonePattern,
-            matrix_def = "+__(___) ___-__-__",
+            matrix_def = "+___(__) ___-__-__",
             matrix = pattern ? pattern : matrix_def,
             i = 0,
             def = matrix.replace(/\D/g, ""),
             val = e.target.value.replace(/\D/g, "");
 
-        if (clearVal !== "false" && e.type === "blur") {
-            if (val.length < matrix.match(/([\_\d])/g).length) {
-                e.target.value = "";
-                return;
-            }
-        }
         if (def.length >= val.length) val = def;
         e.target.value = matrix.replace(/./g, function (e) {
             return /[_\d]/.test(e) && i < val.length
@@ -25,8 +18,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     };
     const numbers = document.querySelectorAll("[data-phone-pattern]");
+
     for (let number of numbers) {
-        for (let e of ["input", "blur", "focus"]) {
+        for (let e of ["input"]) {
             number.addEventListener(e, eventCallback);
         }
     }
