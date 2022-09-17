8cf6d75505bf8b7aa5a5aa9cb9b2c279210160f5
Alexander Kostin <asurv2@gmail.com>
2022-09-17 20:50:13 +0300

    add files

---
+++ build/js/input.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // input.onclick = input.value = "+";
+
+    const eventCallback = function (e) {
+        let el = e.target,
+            pattern = el.dataset.phonePattern,
+            matrix_def = "+___(__) ___-__-__",
+            matrix = pattern ? pattern : matrix_def,
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = e.target.value.replace(/\D/g, "");
+
+        if (def.length >= val.length) val = def;
+        e.target.value = matrix.replace(/./g, function (e) {
+            return /[_\d]/.test(e) && i < val.length
+                ? val.charAt(i++)
+                : i >= val.length
+                ? ""
+                : e;
+        });
+    };
+    const numbers = document.querySelectorAll("[phone-pattern]");
+
+    for (let number of numbers) {
+        for (let e of ["input"]) {
+            number.addEventListener(e, eventCallback);
+        }
+    }
+});
